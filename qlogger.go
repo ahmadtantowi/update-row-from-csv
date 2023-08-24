e812f0158401346a775eb1fe5cbab44a2ebdd2aa
Ahmad Tantowi <ahmad.tantowi@efishery.com>
2023-08-24 08:47:35 +0700

    Add option to log query or not

--- qlogger.go
+++ qlogger.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v5/tracelog"
 )
@@ -10,7 +11,7 @@ import (
 type qLogger struct{}
 
 func (q *qLogger) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]any) {
-	if level == tracelog.LogLevelInfo && msg == "Query" {
+	if msg == "Query" && os.Getenv("LOG_QUERY") == "true" {
 		log.Printf("SQL:\n%s\nARGS:%v\n", data["sql"], data["args"])
 	}
 }
